ae093ff18c61cd0d88a5bf2f37897af364e1808f
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-04-01 00:54:00 -0300

    feat: Criando Repositorio

---
+++ src/main/java/com/angularbackend/backendangular/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.angularbackend.backendangular.repositories;
+
+import com.angularbackend.backendangular.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository {
+    Optional<User> findByEmail(String email);
+}
