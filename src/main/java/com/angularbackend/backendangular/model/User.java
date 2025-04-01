3cdc5d07ac43e9c456fbc59294f63c12f996353b
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-04-01 00:52:10 -0300

    feat: Implementando modelo da entidade

---
+++ src/main/java/com/angularbackend/backendangular/model/User.java
@@ -0,0 +1,52 @@
+package com.angularbackend.backendangular.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    @Size(min = 8)
+    private String password;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
