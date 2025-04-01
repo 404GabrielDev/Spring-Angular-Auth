9785f6ffeab44ae98bab22a138dfe673a4b72fe9
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-03-31 23:52:14 -0300

    feat: commit initial

---
+++ src/main/java/com/angularbackend/backendangular/ProjetoFullStackComAngularApplication.java
@@ -0,0 +1,13 @@
+package com.angularbackend.backendangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFullStackComAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFullStackComAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angularbackend/backendangular/ProjetoFullStackComAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularbackend.backendangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFullStackComAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
